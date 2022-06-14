6c8900c9149e40ed786a0b852ae0ce5a2e4c57e2
korzhenkovitalii <korzhenko.vitalii@gmail.com>
2022-06-14 23:13:22 +0300

    add portfolio

--- js/modal.js
+++ js/modal.js
@@ -14,9 +14,10 @@
 
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  
-  menuBtnRef.addEventListener('click', () => {
+
+  menuBtnRef.addEventListener('click', function () {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
     menuBtnRef.classList.toggle('is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
